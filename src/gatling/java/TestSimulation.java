487c9a5e27687eb6d5fc1ac941386d0193304612
Szymon Pałka <szymon.palka02@gmail.com>
2024-05-25 18:41:00 +0200

    before test

--- src/gatling/java/TestSimulation.java
+++ src/gatling/java/TestSimulation.java
@@ -10,7 +10,7 @@ import static io.gatling.javaapi.http.HttpDsl.status;
 
 public class TestSimulation extends Simulation {
 
-    private static final String BASE_URL = "http://18.159.59.32:8080";
+    private static final String BASE_URL = "";
     HttpProtocolBuilder httpProtocol = http
             .baseUrl(BASE_URL)
             .acceptHeader("application/json")
@@ -18,12 +18,10 @@ public class TestSimulation extends Simulation {
 
     ScenarioBuilder scn = scenario("Create new product with random data")
             .exec(session -> {
-                // Generate random data for each request
                 String name = "Product_" + ThreadLocalRandom.current().nextInt(1, 9999999);
                 String description = "Description_" + name;
                 double price = ThreadLocalRandom.current().nextDouble(10.0, 10000.0);
 
-                // Properly formatted JSON payload
                 String jsonPayload = "{\"name\": \"" + name + "\", " +
                         "\"description\": \"" + description + "\", " +
                         "\"category\": \"Laptop\", " +
@@ -39,6 +37,6 @@ public class TestSimulation extends Simulation {
 
 
     public TestSimulation() {
-        this.setUp(scn.injectOpen(nothingFor(5), constantUsersPerSec(100).during(6000)).protocols(httpProtocol));
+        this.setUp(scn.injectOpen(constantUsersPerSec(200).during(500)).protocols(httpProtocol));
     }
 }
